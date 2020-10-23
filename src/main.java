ff1faedfabf1f263aea908988f1b99f773cbc465
Nikhil S N <73348331+nikhil02nayak@users.noreply.github.com>
2020-10-23 19:09:50 +0530

    Add files via upload

---
+++ src/main.java
@@ -0,0 +1,6 @@
+public class main {
+    public static void main(String[] args) {
+          //double miles = SpeedConverter.toMilesPerHour(10.5);
+        SpeedConverter.printConversion(25.42);
+    }
+}
